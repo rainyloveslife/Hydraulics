7d70f1879ebe84ee65a58f5277b3e6972cf45499
rainyloveslife <69226336+rainyloveslife@users.noreply.github.com>
2025-06-23 14:15:21 +0200

    Add files via upload

---
+++ Forcing data/ERA5 download/ERA5_Apelern.slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=era5_download
+#SBATCH --output=era5_download_%j.log
+#SBATCH --error=era5_download_%j.err
+#SBATCH --time=7-00:00:00            # 运行最长12小时，根据需要调整
+#SBATCH --ntasks=1                  # 只运行一个任务
+#SBATCH --cpus-per-task=4           # 需要2个CPU核心
+#SBATCH --mem=8G                    # 需要4GB内存
+
+# 激活conda环境，如果需要
+# source /opt/ohpc/pub/apps/Miniconda3/4.9.2/etc/profile.d/conda.sh
+# conda activate base
+
+python "/Net/Groups/BSI/work_scratch/zduanmu/phd/ERA5_forcing/ERA5_raw/Ape_download.py" 
---
+++ Forcing data/ERA5 download/Har_download.py
@@ -0,0 +1,47 @@
+import cdsapi
+import os
+
+# 初始化客户端
+client = cdsapi.Client()
+
+dataset = "reanalysis-era5-single-levels"
+
+save_dir = "/Net/Groups/BSI/work_scratch/zduanmu/phd/ERA5_forcing/ERA5_raw/Hartheim"
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+variables = [
+    "surface_pressure"
+]
+
+days = [f"{d:02d}" for d in range(1, 32)]
+times = [f"{h:02d}:00" for h in range(24)]
+
+area = [48.5, 7.1, 47.5, 8.1]
+
+for year in range(2000, 2025):  # 2000到2024年
+    for variable in variables:
+        for month in range(1, 13):
+            month_str = f"{month:02d}"
+            print(f"Downloading {variable} for {year}-{month_str} ...")
+
+            request = {
+                "product_type": "reanalysis",
+                "variable": [variable],
+                "year": str(year),
+                "month": [month_str],
+                "day": days,
+                "time": times,
+                "data_format": "netcdf",
+                "area": area
+            }
+
+            target_file = os.path.join(save_dir, f"Hartheim_{variable}_{year}_{month_str}.nc")
+
+            try:
+                result = client.retrieve(dataset, request)
+                result.download(target_file)
+                print(f"Downloaded {target_file}")
+            except Exception as e:
+                print(f"Failed to download {variable} for {year}-{month_str}: {e}")
